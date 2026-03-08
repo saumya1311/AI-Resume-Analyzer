20753e266a622cebffa0e3ea933a1ec2f7317fd5
Saumya <saumyathakurr1311@gmail.com>
2026-03-09 03:38:06 +0530

    Review Feature Updated

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -78,7 +78,10 @@ const resume = () => {
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
                             <Summary feedback={feedback} />
-                            <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
+                            <ATS 
+                                score={feedback.ATS?.score || 0} 
+                                suggestions={feedback.ATS?.tips || []} 
+                            />
                             <Details feedback={feedback} />
                         </div>
                     ) : (
